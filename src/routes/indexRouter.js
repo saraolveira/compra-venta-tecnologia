f85c771fa1d5ac3c9f26dd9515dbcfedfe2d6fcc
Natalia Gutierrez Puerta <nataliagutierrezpuerta@gmail.com>
2025-02-16 18:03:41 +0100

    actualizacion router

--- src/routes/indexRouter.js
+++ src/routes/indexRouter.js
@@ -1,12 +1,12 @@
 import express from "express";
 
-import { articulosRoutes } from "./articulosRoutes.js";
+import { articulosRouter } from "./articulosRouter.js";
 import { usuariosRouter } from "./usuariosRoutes.js";
 import { solicitudesRouter } from "./solicitudesRoutes.js";
 import { ventasRouter } from "./ventasRoutes.js";
 export const router = express.Router();
 
-router.use(articulosRoutes);
+router.use(articulosRouter);
 router.use(usuariosRouter);
 router.use(solicitudesRouter);
 router.use(ventasRouter);
