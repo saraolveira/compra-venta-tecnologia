e8b0791017110651dcf6659217bb61fec1ead93c
Sara Olveira <93060063+saraolveira@users.noreply.github.com>
2025-02-18 14:55:40 +0100

    Mover archivos a sus carpetas

--- src/controllers/articulos/publishArticuloController.js
+++ src/controllers/articulos/publishArticuloController.js
@@ -2,20 +2,16 @@ import { publishArticleService } from "../../services/articulos/publishArticleSe
 
 export const publishArticleController = async (req, res, next) => {
     try {
-
-        // id articulo
         const { id } = req.params;
 
-        const { nombre, categoria, localidad, precio, descripcion } = req.body
-        const articulo = await publishArticleService(id)
+        const articulo = await publishArticleService(id);
 
         res.status(200).send({
             status: "ok",
             message: "Articulo publicado",
             data: { articulo },
         });
+    } catch (error) {
+        next(error);
     }
-    catch (error) {
-        next(error)
-    }
-}
+};
--- src/services/articulos/publishArticleService.js
+++ src/services/articulos/publishArticleService.js
@@ -2,16 +2,14 @@ import { publishArticuloModel } from "../../models/articulos/publishArticuloMode
 import { generateErrorUtils } from "../../utils/helpersUtils.js";
 
 export const publishArticleService = async (articuloId) => {
-    
     const result = await publishArticuloModel(articuloId);
-    
-    if (result.affectedRows !== 1) {
-       throw generateErrorUtils(
-           500,
-           "SOLICITUD_NOT_MADE",
-           "No se ha podido publicar el  articulo"
-       );
-    }
-    return result;
-};
 
+    if (result.affectedRows !== 1) {
+        throw generateErrorUtils(
+            500,
+            "SOLICITUD_NOT_MADE",
+            "No se ha podido publicar el  articulo"
+        );
+    }
+    return articuloId;
+};
